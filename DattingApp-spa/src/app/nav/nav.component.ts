b65d617e3adf6e14147e1e51725919fde3ca76c7
ganeshkm <g.kadam@easternenterprise.com>
2019-03-13 17:09:24 +0530

    Adding 3rd party components to our app

--- DattingApp-spa/src/app/nav/nav.component.ts
+++ DattingApp-spa/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -10,28 +11,26 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor(private auth: AuthService) { }
+  constructor(public authService: AuthService, private alertifyService: AlertifyService) { }
 
   ngOnInit() {
   }
 
   login() {
-    this.auth.login(this.model).subscribe(next => {
-      console.log('Login successfully');
+    this.authService.login(this.model).subscribe(next => {
+      this.alertifyService.success('Logged in successfully');
     }, error => {
-      console.log('Login failed');
+      this.alertifyService.error(error.statusText);
     }
     );
   }
 
   loggedIn() {
-    const token = localStorage.getItem('token');
-    return !!token;
+    return this.authService.loggedIn();
   }
 
   logout() {
     localStorage.removeItem('token');
-    console.log('Logged out');
-
+    this.alertifyService.message('Logged out');
   }
 }
